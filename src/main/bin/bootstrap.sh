ee700827fd1dc57ba0531f4c3f507a09176c3312
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-11-07 10:51:11 +0100

    Revert "adding group for JMS messaging"
    This reverts commit ac6e6c7f07c3fc42604f7732e2f1ff6f8a050592.

--- src/main/bin/bootstrap.sh
+++ src/main/bin/bootstrap.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
-_target_base="$(pwd)"
-_target="${_target_base}/fiteagle"
+_base="$(pwd)"
 
 _container_type="wildfly"
 _container_version="8.0.0.Beta1"
 _container_file="${_container_type}-${_container_version}.zip"
 _container_url="http://download.jboss.org/${_container_type}/${_container_version}/${_container_file}"
-_container_folder="${_target_base}/container"
+_container_folder="${_base}/container"
 _container_config="wildfly-standalone.xml"
+_container_config_url="https://raw.github.com/tubav/fiteagle_osgi/master/src/main/resources/wildfly-standalone.xml"
 
 _osgi_file="jbosgi-installer-2.1.0.jar"
 _osgi_url="http://sourceforge.net/projects/jboss/files/JBossOSGi/2.1.0/${_osgi_file}/download"
 _osgi_config="jbosgi-autoinstall.xml"
+_osgi_config_url="https://raw.github.com/tubav/fiteagle_osgi/master/src/main/resources/jbosgi-autoinstall.xml"
 
-_installer_folder="${_target_base}/tmp"
-_installer_resources="$(pwd)/src/main/resources"
+_installer_folder="${_base}/tmp"
+
+_src_folder="${_base}/src"
 
 _wildfly_admin_user="admin"
 _wildfly_admin_pwd="admin"
 _wildfly_app_user="fiteagle"
 _wildfly_app_pwd="fiteagle"
-_wildfly_app_group="guest"
 
 function checkBinary {
   echo -n " * Checking for '$1'..."
@@ -49,15 +50,17 @@ function installOSGi() {
     curl -C - -fsSkL -o "${_installer_folder}/${_osgi_file}" "${_osgi_url}"
     echo "Installing OSGi..."
     mkdir -p "${_installer_folder}"
-    java -jar "${_installer_folder}/${_osgi_file}" "${_installer_resources}/${_osgi_config}"
+    curl -C - -fsSSkL -o "${_installer_folder}/${_osgi_config}" "${_osgi_config_url}"
+    java -jar "${_installer_folder}/${_osgi_file}" "${_installer_folder}/${_osgi_config}"
 }
 
 function configContainer() {
     echo "Configuring container..."
-    cp "${_installer_resources}/${_container_config}" "${_container_folder}/${_container_type}-${_container_version}/standalone/configuration"
+    curl -C - -fsSSkL -o "${_installer_folder}/${_container_config}" "${_container_config_url}"
+    cp "${_installer_folder}/${_container_config}" "${_container_folder}/${_container_type}-${_container_version}/standalone/configuration"
     cd "${_container_folder}/${_container_type}-${_container_version}"
     ./bin/add-user.sh -u "${_wildfly_admin_user}" -p "${_wildfly_admin_pwd}"
-    ./bin/add-user.sh -a -g "${_wildfly_app_group}" -u "${_wildfly_app_user}" -p "${_wildfly_app_pwd}"
+    ./bin/add-user.sh -a -u "${_wildfly_app_user}" -p "${_wildfly_app_pwd}"
 }
 
 
@@ -84,15 +87,15 @@ function checkEnvironment {
 
 
 function installFITeagle {
-  repo="fiteagle"
+  repo="fiteagle_osgi"
   git_url="git://github.com/tubav/${repo}"
   
-  if [ -d "${_target}/.git" ]; then
-    echo -n "Updating FITeagle..."
-    (cd "${_target}" && git pull -q)
+  if [ -d "${_src_folder}/.git" ]; then
+    echo -n "Updating FITeagle sources..."
+    (cd "${_src_folder}" && git pull -q)
   else
-    echo -n "Installing FITeagle..."
-    git clone -q --recursive --depth 1 ${git_url} ${_target}
+    echo -n "Getting FITeagle sources..."
+    git clone -q --recursive --depth 1 ${git_url} ${_src_folder}
   fi
   if [ "0" != "$?" ]; then
     echo >&2 "FAILED. Please have a look above."
@@ -109,10 +112,3 @@ installOSGi
 configContainer
 installFITeagle
 startContainer
-
-
-#cd ${_target}
-#./src/main/bin/fiteagle.sh test
-#./src/main/bin/fiteagle.sh start
-
-

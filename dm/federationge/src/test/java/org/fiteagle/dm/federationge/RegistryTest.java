32fd616b9eca1f45ee9b5074a8a2198b7c81fa53
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-12-12 09:04:43 +0100

    adding Federation GE

---
+++ dm/federationge/src/test/java/org/fiteagle/dm/federationge/RegistryTest.java
@@ -0,0 +1,37 @@
+package org.fiteagle.dm.federationge;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RegistryTest {
+
+	private Registry registry;
+	String input = "test";
+	String version = "v1";
+	String regionid = "1";
+	String serviceid = "1";
+	
+	@Before
+	public void setup() {
+		this.registry = new Registry();
+	}
+	
+	@Test
+	public void testEchoMessageJSON() {
+		
+		String output = this.registry.echoMessageJSON(version, regionid, serviceid);
+		Assert.assertTrue(output.startsWith("{"));
+		Assert.assertTrue(output.contains(serviceid));
+	}
+
+	@Test
+	public void testEchoMessageXML() {
+		
+		String output = this.registry.echoMessageXML(version, regionid, serviceid);
+		Assert.assertTrue(output.startsWith("<xml"));
+		Assert.assertTrue(output.contains(serviceid));
+	}
+
+}

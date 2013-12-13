87e6c351e35c78ce057e603c454f8ae91c528748
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-12-13 09:36:43 +0100

    layouting and working on data object

--- dm/fantaastic/src/main/webapp/status/js/fantaastic.js
+++ dm/fantaastic/src/main/webapp/status/js/fantaastic.js
@@ -1,45 +1,75 @@
+// to be changed to real data
+function getData() {
+	ple = {
+            title: "PlanetLab Europe",
+            type: "check",
+            color: "success",
+            long: "Running normally",
+            short: "UP",
+            id: "ple",
+            message: "No issue reported or monitored"
+	};
+	fpg = {
+            title: "FUSECO Playground",
+            type: "check",
+            color: "success",
+            long: "Running normally",
+            short: "UP",
+            id: "fpg",
+            message: "No issue reported or monitored"
+	};
+	data = {'ple': ple,
+			'fpg': fpg};
+	return data;
+}
+
+
 function showContent(name) {
     data = { };
     $('#content').html(tmpl(name)(data));
 }
 
+
 function showTestbed(name, data) {
     $('#content').html(tmpl(name)(data));
 }
 
+
+function showTestbedCard(name, data) {
+    $('#testbedCards').append(tmpl(name)(data));
+}
+
 function showTab() {
-    hash = window.location.hash;
+	var data = getData();
+	var hash = window.location.hash;
     $('li.active').removeClass('active')
+
+    for (var prop in data) {
+    	if (hash == "#"+prop) {
+    	    $('.'+prop+'btn').parent().addClass('active')
+    	    showTestbed("tmplTestbed", data[prop]);
+    	    return;
+    	}
+    }
+
     if (hash == "#history") {
         $('.historybtn').parent().addClass('active')
         showContent("tmplHistory");
     } else if (hash == "#report") {
         $('.reportbtn').parent().addClass('active')
         showContent("tmplReport");
-    } else if (hash == "#ple") {
-        $('.plebtn').parent().addClass('active')
-        data = {
-            title: "PlanetLab Europe",
-            type: "check",
-            color: "success",
-            message: "Running normally"
-        }
-        showTestbed("tmplTestbed", data);
-    } else if (hash == "#fpg") {
-        $('.fpgbtn').parent().addClass('active')
-        data = {
-            title: "FUSECO Playground",
-            type: "check",
-            color: "success",
-            message: "Running normally"
-        }
-        showTestbed("tmplTestbed", data);
     } else {
         showContent("tmplDashboard");
+        for (var prop in data) {
+        	showTestbedCard("tmplTestbedBoard", data[prop]);
+        }
         $('.dashboardbtn').parent().addClass('active')
     }
 }
 
-window.onhashchange = showTab;
-if (window.location.hash == "") window.location.hash="#dashboard";
-showTab();
+    
+$(document).ready(function() {
+	window.onhashchange = showTab;
+	if (window.location.hash == "") window.location.hash="#dashboard";
+	showTab();
+});
